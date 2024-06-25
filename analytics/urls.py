932ab754af28ebc79aa8bb85282ee0d7aa48eb6b
Адилан Ахрамович <a.akhramovich@almatydc.kz>
2024-06-25 09:21:42 +0500

    fixed a bug

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import get_analytics_data
+from .views import get_analytics_data, update_analytics_data
 
 urlpatterns = [
     path('analytics-data/', get_analytics_data, name='analytics-data'),
+    path('update-analytics/', update_analytics_data, name='update-analytics'),
 ]
