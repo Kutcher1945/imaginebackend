c911c7e631888ce9ac6f6aa24ac2ede40f0fdd53
Адилан Ахрамович <a.akhramovich@almatydc.kz>
2024-06-25 09:37:07 +0500

    fixed a bug

--- imaginebackend/settings.py
+++ imaginebackend/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'analytics',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -48,6 +50,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "https://imagineanalytics.ai",  # Add your frontend URL here
 ]
 
 ROOT_URLCONF = 'imaginebackend.urls'
